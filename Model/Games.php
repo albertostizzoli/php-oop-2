8659a4b753e22122839482f7f8de57307b916da8
albertostizzoli <albertostizzoli@yahoo.it>
2023-12-05 15:04:13 +0100

    Create class Games

---
+++ Model/Games.php
@@ -0,0 +1,28 @@
+<?php
+
+class Games
+{
+    private string $name;
+    private string $image_icon_url;
+
+    private function __construct($title, $image)
+    {
+        $this->name = $title;
+        $this->image_icon_url = $image;
+    }
+
+    private function printGame()
+    {
+        $title = $this->name;
+        $image = $this->image_icon_url;
+    }
+}
+
+$gameString = file_get_contents(__DIR__ . "/steam_db.json");
+$gameList = json_decode($gameString, true);
+
+$games = [];
+foreach ($gameList as $item) {
+    $games[] = new Games($item['name'], $item['image_icon_url']);
+}
+?>
