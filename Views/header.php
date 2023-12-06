7df75f693c85f1d929ba1dcfa46151ecd3430a81
albertostizzoli <albertostizzoli@yahoo.it>
2023-12-06 16:35:16 +0100

    Changed navbar

--- Views/header.php
+++ Views/header.php
@@ -18,16 +18,20 @@
         <h1 class="text-center text-white text-bg-danger p-3">OOP - Shop</h1>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="movie.php">MOVIES</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="book.php">BOOK</a>
+                            <a class="nav-link active" href="book.php">BOOKS</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="game.php">GAMES</a>
+                            <a class="nav-link active" href="game.php">GAMES</a>
                         </li>
                     </ul>
                 </div>
